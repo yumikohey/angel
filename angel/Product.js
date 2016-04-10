128c88593ee7656d34760d7af32050c17c2be456
Xi Huang <dayumikoda@gmail.com>
2016-04-10 12:49:17 -0700

    more for product page

--- angel/Product.js
+++ angel/Product.js
@@ -4,6 +4,7 @@ const {
     View,
     TouchableHighlight,
     Text,
+    Image,
 } = React;
 
 const styles = React.StyleSheet.create({
@@ -36,6 +37,55 @@ const styles = React.StyleSheet.create({
         fontSize: 20,
         fontWeight: '600',
     },
+    feed: {
+        paddingTop: 20,
+        flexDirection: 'row',
+    },
+    feedImg: {
+        margin: 5,
+        width: 325,
+        height: 280,
+    },
+    labelRow: {
+        marginTop: 10,
+        flex: 2,
+        alignItems: 'flex-end',
+    },
+    detailRow: {
+        marginTop: 10,
+        flex: 2,
+        alignItems: 'flex-start',
+    },
+    brandLabel: {
+        width: 120,
+        borderRadius: 2,
+        borderColor: '#57e7fd',
+        borderWidth: 2,
+        backgroundColor: '#fff',
+        paddingLeft: 15,
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingRight: 15,
+    },
+    buyButton: {
+        marginRight: 2,
+        padding:10,
+        backgroundColor: '#57e7fd',
+        borderColor: '#57e7fd',
+        color: '#555',
+        fontWeight: '600',
+        borderRadius: 10,
+    },
+    feedContainer: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#E7E7E7',
+        padding: 20,
+        marginBottom: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        height: 450,
+    },
 });
 
 class Product extends React.Component {
@@ -51,6 +101,27 @@ class Product extends React.Component {
                         Angel
                     </Text>
                 </TouchableHighlight>
+                <View style={styles.feedContainer}>
+                    <View style={styles.feed}>
+                        <Image
+                            style={styles.feedImg}
+                            source={require('./img/mk_1.png')}/>
+                    </View>
+                    <View style={styles.detailRow}>
+                        <Text style={styles.buyButton}>
+                            CONFIRM
+                        </Text>
+                    </View>
+                    <View style={styles.labelRow}>
+                        <TouchableHighlight
+                            onPress={this.props.productPage}
+                            >
+                            <Text style={styles.buyButton}>
+                                CONFIRM
+                            </Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
             </View>
         );
     }
